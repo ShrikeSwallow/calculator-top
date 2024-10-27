6f548bf105a0890023c76c856b3f742fbfa7ba24
Maciej Jaskolka <maciej.jaskolka@gmail.com>
2024-10-27 09:12:36 +0000

    Fix module/import issues across the project

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
-import { add, subtract, multiply, divide } from "./operations.js";
+import { operate } from "./operations.js";
 
 let numberOne = null;
 let numberTwo = null;
 let operator = "";
-
-const operate = (numberOne, numberTwo, operator) => {
-  switch (operator) {
-    case "+":
-      add(numberOne, numberTwo);
-    case "-":
-      subtract(numberOne, numberTwo);
-    case "*":
-      multiply(numberOne, numberTwo);
-    case "/":
-      divide(numberOne, numberTwo);
-  }
-};
--- operations.js
+++ operations.js
@@ -1,17 +1,41 @@
-export const add = (a, b) => {
+const add = (a, b) => {
   return a + b;
 };
 
-export const subtract = (a, b) => {
+const subtract = (a, b) => {
   return a - b;
 };
 
-export const multiply = (a, b) => {
+const multiply = (a, b) => {
   return a * b;
 };
 
-export const divide = (a, b) => {
+const divide = (a, b) => {
   if (b === 0) {
     return "Don't be silly";
   } else return a / b;
+};
+
+const percentage = (a, b) => {
+  return (a * b) / 100;
+};
+
+const squareRoot = (a) => {
+  return Math.sqrt(a);
+};
+export const operate = (numberOne, numberTwo, operator) => {
+  switch (operator) {
+    case "+":
+      add(numberOne, numberTwo);
+    case "-":
+      subtract(numberOne, numberTwo);
+    case "*":
+      multiply(numberOne, numberTwo);
+    case "/":
+      divide(numberOne, numberTwo);
+    case "%":
+      percentage(numberOne, numberTwo);
+    case "√":
+      squareRoot(numberOne);
+  }
 };
