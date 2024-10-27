fec66ad6644696f11373656445dd1d0964033724
Maciej Jaskolka <maciej.jaskolka@gmail.com>
2024-10-27 19:41:00 +0000

    Refactor the code and fix minor bugs

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-import { operate } from "./operations.js";
-
 const buttons = document.querySelectorAll("button");
-// const numbers = buttons.querySelectorAll(".numberBtn");
-// const operators = document.querySelectorAll(".functionBtn");
 const displayText = document.querySelector("#display-text");
+const operators = ["+", "-", "*", "/", "%", "√"];
 
 let numberOne = null;
 let numberTwo = null;
@@ -16,7 +13,6 @@ const reinitAll = () => {
   numberTwo = null;
   operator = "";
   displayScreen = "";
-  displayText.textContent = "0";
 };
 
 const checkFloatPoint = () => {
@@ -37,8 +33,6 @@ const concNumber = (number) => {
   }
 };
 
-//const render = () => {
-
 buttons.forEach((button) => {
   button.addEventListener("click", (event) => {
     const clicked = event.currentTarget;
@@ -52,9 +46,14 @@ buttons.forEach((button) => {
         concNumber(clicked.textContent);
       }
       displayText.textContent = `${displayScreen}`;
-    } else if (clicked.getAttribute("id") === "clearBtn") {
+    }
+
+    if (clicked.getAttribute("id") === "clearBtn") {
       reinitAll();
-    } else if (clicked.getAttribute("id") === "backspaceBtn") {
+      displayText.textContent = "0";
+    }
+
+    if (clicked.getAttribute("id") === "backspaceBtn") {
       if (displayScreen.substring(0, displayScreen.length - 1) === "") {
         displayScreen = "0";
       } else {
@@ -62,9 +61,87 @@ buttons.forEach((button) => {
       }
       displayText.textContent = `${displayScreen}`;
     }
+
+    if (operators.includes(clicked.textContent)) {
+      if (!numberOne) {
+        numberOne = convertDisplayToNumber(displayScreen);
+        operator = clicked.textContent;
+        if (operator === "√") {
+          //let resultStrLength = Math.round(squareRoot(numberOne)).toString().length;
+          displayText.textContent = `${squareRoot(numberOne).toPrecision(10)}`;
+          reinitAll();
+        }
+      }
+      displayScreen = "";
+    }
+
+    if (clicked.getAttribute("id") === "operateBtn") {
+      if (numberOne && operator !== "") {
+        numberTwo = convertDisplayToNumber(displayScreen);
+
+        operate(numberOne, numberTwo, operator);
+        reinitAll();
+      } else {
+        return;
+      }
+    }
   });
 });
 
-//};
+const convertDisplayToNumber = (number) => {
+  if (number.includes(".")) {
+    return Number.parseFloat(number);
+  } else {
+    return Number.parseInt(number);
+  }
+};
 
-//render();
+const operate = (a, b, operator) => {
+  switch (operator) {
+    case "+":
+      displayText.textContent = `${add(a, b)}`;
+      break;
+    case "-":
+      displayText.textContent = `${subtract(a, b)}`;
+      break;
+    case "*":
+      displayText.textContent = `${multiply(a, b)}`;
+      break;
+    case "/":
+      if ((divide(a, b) % 1).toString.length > 6) {
+        displayText.textContent = `${divide(a, b).toPrecision(10)}`;
+      } else {
+        displayText.textContent = `${divide(a, b)}`;
+      }
+      break;
+    case "%":
+      displayText.textContent = `${percentage(a, b)}`;
+      break;
+  }
+};
+
+const add = (a, b) => {
+  return a + b;
+};
+
+const subtract = (a, b) => {
+  return a - b;
+};
+
+const multiply = (a, b) => {
+  return a * b;
+};
+
+const divide = (a, b) => {
+  if (b === 0) {
+    return "Don't be silly";
+  } else return a / b;
+};
+
+const percentage = (a, b) => {
+  return (a * b) / 100;
+};
+
+const squareRoot = (a) => {
+  return Math.sqrt(a);
+};
